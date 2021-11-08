812572840ee71ef36443631544f7f11c30ab627d
Melanga <dissanayakedmmb@gmail.com>
2021-11-08 23:25:10 +0530

    Added more media quearies to Timeline componenet wrapper

---
+++ src/components/Sections/EducationSection/EducationSectionElements.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const EducationTimelineWrapper = styled.div`
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 500px;
+  height: 400px;
+  @media screen and (max-width: 530px) {
+    width: 380px;
+  }
+  @media screen and (max-width: 480px) {
+    width: 340px;
+  }
+  @media screen and (max-width: 370px) {
+    width: 300px;
+  }
+`;
