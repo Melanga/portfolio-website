b78df83b034c63c337d165893d4487de9472f26c
Melanga Dissanayake <dissanayakedmmb@gmail.com>
2021-11-16 23:13:14 +0530

    converted into function instead of class

--- src/components/SideNavBar/index.jsx
+++ src/components/SideNavBar/index.jsx
@@ -10,39 +10,35 @@ import {
   SideNavBtn,
 } from "./SideNavBarElements";
 
-class SideNavBar extends React.Component {
-  render() {
-    const { isOpen, onToggle } = this.props;
-
-    return (
-      <SideNavBarContainer isOpen={isOpen} onClick={onToggle}>
-        <Icon onClick={onToggle}>
-          <CloseIcon></CloseIcon>
-        </Icon>
-        <SideNavBarWrapper>
-          <SideNavBarMenu>
-            <SideNavBarLink to="about_me" onClick={onToggle}>
-              About Me
-            </SideNavBarLink>
-            <SideNavBarLink to="education" onClick={onToggle}>
-              Education
-            </SideNavBarLink>
-            <SideNavBarLink to="projects" onClick={onToggle}>
-              Projects
-            </SideNavBarLink>
-            <SideNavBarLink to="technologies" onClick={onToggle}>
-              Technologies
-            </SideNavBarLink>
-          </SideNavBarMenu>
-          <SideNavBtnWrap>
-            <SideNavBtn to="footer" onClick={onToggle}>
-              Contact Me
-            </SideNavBtn>
-          </SideNavBtnWrap>
-        </SideNavBarWrapper>
-      </SideNavBarContainer>
-    );
-  }
-}
+const SideNavBar = ({ isOpen, toggleIsOpen }) => {
+  return (
+    <SideNavBarContainer isOpen={isOpen} onClick={toggleIsOpen}>
+      <Icon onClick={toggleIsOpen}>
+        <CloseIcon></CloseIcon>
+      </Icon>
+      <SideNavBarWrapper>
+        <SideNavBarMenu>
+          <SideNavBarLink to="about_me" onClick={toggleIsOpen}>
+            About Me
+          </SideNavBarLink>
+          <SideNavBarLink to="education" onClick={toggleIsOpen}>
+            Education
+          </SideNavBarLink>
+          <SideNavBarLink to="projects" onClick={toggleIsOpen}>
+            Projects
+          </SideNavBarLink>
+          <SideNavBarLink to="technologies" onClick={toggleIsOpen}>
+            Technologies
+          </SideNavBarLink>
+        </SideNavBarMenu>
+        <SideNavBtnWrap>
+          <SideNavBtn to="footer" onClick={toggleIsOpen}>
+            Contact Me
+          </SideNavBtn>
+        </SideNavBtnWrap>
+      </SideNavBarWrapper>
+    </SideNavBarContainer>
+  );
+};
 
 export default SideNavBar;
