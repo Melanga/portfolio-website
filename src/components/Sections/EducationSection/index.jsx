2a38c5753fba060c48ff126f99f1e45869604cc3
Melanga Dissanayake <dissanayakedmmb@gmail.com>
2021-12-03 21:28:18 +0530

    Parallax effect is added to education section

--- src/components/Sections/EducationSection/index.jsx
+++ src/components/Sections/EducationSection/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useInView } from "react-intersection-observer";
 import { useSpring, animated, config } from "react-spring";
+import { ParallaxLayer } from "@react-spring/parallax";
 import useViewport from "../../Hook/ViewPort";
 import {
   SectionContainer,
@@ -29,7 +30,7 @@ const EducationSection = ({
   img,
   alt,
 }) => {
-  const { ref, inView } = useInView({ threshold: 0.3 });
+  const { ref, inView } = useInView({ threshold: 0.4 });
 
   const { width } = useViewport();
   let laptopWidth = width > 1200;
@@ -38,61 +39,72 @@ const EducationSection = ({
     opacity: inView ? 1 : 0,
     marginLeft: laptopWidth ? (inView ? 0 : -400) : 0,
     config: config.gentle,
-    //delay: 300,
+    delay: 200,
   });
 
   const contentTextProps = useSpring({
     opacity: inView ? 1 : 0,
     marginTop: laptopWidth ? (inView ? 0 : -200) : 0,
     config: config.gentle,
-    delay: 100,
+    delay: 300,
   });
 
   const contentHeaderProps = useSpring({
     opacity: inView ? 1 : 0,
     marginTop: laptopWidth ? (inView ? 0 : -200) : 0,
     config: config.slow,
-    delay: 100,
+    delay: 300,
   });
 
   const contentTimelineProps = useSpring({
     opacity: inView ? 1 : 0,
     marginTop: laptopWidth ? (inView ? 0 : -200) : 0,
     config: config.wobbly,
-    delay: 100,
+    delay: 300,
   });
 
   return (
     <React.Fragment>
       <SectionContainer ref={ref} lightBg={lightBg} id={id}>
         <SectionWrapper>
-          <SectionRow imgStart={imgStart}>
-            <Column1>
-              <EducationTextWrapper>
-                <animated.div style={contentTextProps}>
-                  <TopLine>{topLine}</TopLine>
-                </animated.div>
-                <animated.div style={contentHeaderProps}>
-                  <Heading lightText={lightText}>{headLine}</Heading>
-                </animated.div>
-                {/* <Subtitle darkText={darkText}>
+          <ParallaxLayer
+            offset={0.99}
+            speed={0.45}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              zIndex: 4,
+            }}
+          >
+            <SectionRow imgStart={imgStart}>
+              <Column1>
+                <EducationTextWrapper>
+                  <animated.div style={contentTextProps}>
+                    <TopLine>{topLine}</TopLine>
+                  </animated.div>
+                  <animated.div style={contentHeaderProps}>
+                    <Heading lightText={lightText}>{headLine}</Heading>
+                  </animated.div>
+                  {/* <Subtitle darkText={darkText}>
                   {description}
                 </Subtitle> */}
-              </EducationTextWrapper>
-              <animated.div style={contentTimelineProps}>
-                <EducationTimelineWrapper>
-                  <EducationTimeline vertical={false} />
-                </EducationTimelineWrapper>
-              </animated.div>
-            </Column1>
-            <Column2>
-              <animated.div style={contentImgProps}>
-                <ImgWrap>
-                  <Img src={img} alt={alt} />
-                </ImgWrap>
-              </animated.div>
-            </Column2>
-          </SectionRow>
+                </EducationTextWrapper>
+                <animated.div style={contentTimelineProps}>
+                  <EducationTimelineWrapper>
+                    <EducationTimeline vertical={false} />
+                  </EducationTimelineWrapper>
+                </animated.div>
+              </Column1>
+              <Column2>
+                <animated.div style={contentImgProps}>
+                  <ImgWrap>
+                    <Img src={img} alt={alt} />
+                  </ImgWrap>
+                </animated.div>
+              </Column2>
+            </SectionRow>
+          </ParallaxLayer>
         </SectionWrapper>
       </SectionContainer>
     </React.Fragment>
